ca0eb24cd2df89e237df89d5f8a638028b89c125
Fauzi <fauzigamer@hotmail.com>
2026-09-06 02:25:15 -0300

    feat: add android capacitor support with branded icons and splash

---
+++ android/app/src/main/java/com/forgeboard/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.forgeboard.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.forgeboard.app',
+  appName: 'ForgeBoard',
+  webDir: 'dist'
+};
+
+export default config;
